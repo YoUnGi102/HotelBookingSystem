15dda77c9b2f283b2b2435b05b4440a609aeb4e0
YoUnGi102 <90184551+YoUnGi102@users.noreply.github.com>
2022-05-12 19:55:32 +0200

    Merged branches

--- src/main/java/com/example/hotelbookingsystem/view/ManageGuestViewController.java
+++ src/main/java/com/example/hotelbookingsystem/view/ManageGuestViewController.java
@@ -3,24 +3,16 @@ package com.example.hotelbookingsystem.view;
 import com.example.hotelbookingsystem.model.Address;
 import com.example.hotelbookingsystem.model.Guest;
 import com.example.hotelbookingsystem.viewModel.ManageGuestViewModel;
-import com.example.hotelbookingsystem.viewModel.RoomListViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
-import static com.example.hotelbookingsystem.dao.BookingTable.*;
-import static com.example.hotelbookingsystem.dao.DatabaseConnection.SCHEMA;
+public class ManageGuestViewController implements Controller {
 
-public class ManageGuestViewController implements Controller
-{
-
-
-    @FXML
     private TextField firstName, lastName, eMail, phoneNumber, number, street, city, postCode, country, passNr;
     @FXML
     private Button addBtn, confirmBtn;
@@ -34,43 +26,34 @@ public class ManageGuestViewController implements Controller
     private Controller lastController;
 
 
-    public void init(ViewHandler viewHandler, ManageGuestViewModel viewModel, Region root, Controller lastController)
-    {
+    public void init(ViewHandler viewHandler, ManageGuestViewModel viewModel, Region root, Controller lastController) {
         this.viewHandler = viewHandler;
         this.viewModel = viewModel;
         this.root = root;
-        this.lastController = lastController;
-        this.previousView = lastController;
     }
 
-    public Region getRoot()
-    {
+    public Region getRoot() {
         return root;
     }
 
 
     @FXML
-    void add(ActionEvent event) throws SQLException
-    {
+    void add(ActionEvent event) throws SQLException {
 
-        viewModel.add(new Guest(firstName.getText(), lastName.getText(), new Address(city.getText(), street.getText(),number.getText(), postCode.getText()), phoneNumber.getText(), eMail.getText(), passNr.getText()));
+        viewModel.add(new Guest(firstName.getText(), lastName.getText(), new Address(city.getText(), street.getText(), number.getText(), postCode.getText()), phoneNumber.getText(), eMail.getText(), passNr.getText()));
     }
 
     @FXML
-    void back(ActionEvent event)
-    {
-        if(lastController instanceof GuestListViewController)
-        viewHandler.openView(ViewHandler.GUEST_LIST_VIEW, this);
+    void back(ActionEvent event) {
+        if (lastController instanceof GuestListViewController)
+            viewHandler.openView(ViewHandler.GUEST_LIST_VIEW, this);
         else
             viewHandler.openView(ViewHandler.MANAGE_BOOKING_VIEW, this);
     }
 
     @FXML
-    void confirm(ActionEvent event)
-    {
+    void confirm(ActionEvent event) {
         viewHandler.openView(ViewHandler.GUEST_LIST_VIEW, this);
     }
 
-
 }
-
---
+++ src/main/java/com/example/hotelbookingsystem/viewModel/ManageGuestViewModel.java
@@ -0,0 +1,18 @@
+package com.example.hotelbookingsystem.viewModel;
+
+import com.example.hotelbookingsystem.model.Guest;
+import com.example.hotelbookingsystem.model.Model;
+
+import java.sql.SQLException;
+
+public class ManageGuestViewModel {
+    private Model model;
+
+    public ManageGuestViewModel(Model model) {
+        this.model = model;
+    }
+
+    public void add(Guest guest) throws SQLException {
+        model.addGuest(guest);
+    }
+}
